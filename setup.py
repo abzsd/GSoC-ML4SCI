ea245da02812b54b659074b6173f6c2503ba71d0
Abhay Kamble <54679619+abzsd@users.noreply.github.com>
2022-10-10 23:31:31 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GSoC - ML4SCI',
+    version='1.0.0',
+    author='Abhay Kamble',
+    author_email='abhaykambley1@gmail.com',
+    description="Quantum Generative Adversarial Neural Networks",
+    long_description="This package is a implementation of different Quantum/ Classical Generative Adversarial Networks, with the dataset being the HEP data.",
+    packages=['QGANHEP'],
+    url='https://github.com/abzsd/GSoC-ML4SCI'
+)
